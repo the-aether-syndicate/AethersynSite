bda8e119f511cea2debd403f37913aa16ac4ef7e
fordav3 <fordav3@gmail.com>
2019-06-01 15:42:29 -0500

    Update users.blade.php

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -85,7 +85,7 @@
                 {
                     processing: true,
                     serverSide: true,
-                    ajax: 'http://triglavdefense/users/get/data',
+                    ajax: '/users/get/data',
                     columns: [
                         {data: 'id', render:
                         function(datafield)
