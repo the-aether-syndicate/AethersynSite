a814d2e3defec3665299742a6ac0926ee0cadb20
fordav3 <fordav3@gmail.com>
2019-06-01 14:22:07 -0500

    Update 2019_05_31_035022_create_doctrine_fitting_tables.php

--- database/migrations/2019_05_31_035022_create_doctrine_fitting_tables.php
+++ database/migrations/2019_05_31_035022_create_doctrine_fitting_tables.php
@@ -13,47 +13,7 @@ class CreateDoctrineFittingTables extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
-            $table->bigInteger('id')->primary();
-            $table->string('name');
-            $table->boolean('active');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-        Schema::create('role', function (Blueprint $table) {
-            $table->increments('id')->primary();
-            $table->string('name');
-            $table->string('description');
-            $table->timestamps();
-        });
-        Schema::create('role_user', function (Blueprint $table) {
-            $table->increments('id')->primary();
-            $table->integer('role_id');
-            $table->integer('user_id');
-            $table->timestamps();
-        });
-        Schema::create('refreshToken', function (Blueprint $table) {
-            $table->bigInteger('character_id')->primary();
-            $table->mediumText('refresh_token');
-            $table->json('scopes');
-            $table->dateTime('expires_on');
-            $table->dateTime('deleted_at');
-            $table->string('token');
-            $table->timestamps();
-        });
-        Schema::create('doctrine', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
-        Schema::create('fitting', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('fitname');
-            $table->string('shiptype');
-            $table->text('eftfitting');
-            $table->integer('doctrine_id');
-            $table->timestamps();
-        });
+        
     }
 
     /**
