97a6263ba17defcfa75b20a9a5db923d1e4a2647
fordav3 <fordav3@gmail.com>
2019-06-03 18:02:30 -0500

    fix 2

--- database/migrations/2019_06_03_230139_fix_fleet_table.php
+++ database/migrations/2019_06_03_230139_fix_fleet_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFleetsTable extends Migration
+class FixFleetTable extends Migration
 {
     /**
      * Run the migrations.
