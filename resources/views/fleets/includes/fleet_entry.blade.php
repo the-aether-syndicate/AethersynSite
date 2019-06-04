4a10c4515f58daa682c19da63106d29e151f7edf
fordav3 <fordav3@gmail.com>
2019-06-03 20:41:59 -0500

    Update fleet_entry.blade.php

--- resources/views/fleets/includes/fleet_entry.blade.php
+++ resources/views/fleets/includes/fleet_entry.blade.php
@@ -7,7 +7,7 @@
         @if($fleet->getfc()->id == auth()->user()->id)
         <a href="{{route('fleet.end', ['fleetid' => $fleet->id])}}" class="btn btn-danger">End Fleet </a>
         @else
-            @if($fleet->punches()->where('user_id', auth()->user()->id))
+            @if($fleet->punches()->where('user_id', auth()->user()->id)->get())
                 <a href="{{route('fleet.leave', ['fleetid' => $fleet->id])}}" class="btn btn-danger">Leave Fleet </a>
                 @else
             <a href="{{route('fleet.join', ['fleetid' => $fleet->id])}}" class="btn btn-success">Join Fleet </a>
