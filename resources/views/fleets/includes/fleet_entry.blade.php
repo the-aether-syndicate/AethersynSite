3a0a5d7a3978b312e3eddffafae55176127a7f34
fordav3 <fordav3@gmail.com>
2019-06-07 20:47:06 -0500

    Update fleet_entry.blade.php

--- resources/views/fleets/includes/fleet_entry.blade.php
+++ resources/views/fleets/includes/fleet_entry.blade.php
@@ -4,13 +4,13 @@
     <td>{{$fleet->created_at}}</td>
     <td>
         @if($fleet->active)
-            @if($fleet->getfc()->id == auth()->user()->id)
+            @if(auth()->user() && auth()->user()->hasRole('Fleet Commander'))
                 <a href="{{route('fleet.end', ['fleetid' => $fleet->id])}}" class="btn btn-danger">End Fleet </a>
 
                 <a href="{{route('fleet.view', ['fleetid' => $fleet->id])}}" class="btn btn-success">View</a>
 
             @else
-                @switch($fleet->hasMember(auth()->user()->id))
+                @switch($fleet->hasMember(auth()->user()->id) )
                     @case(0)
                     <a href="{{route('fleet.join', ['fleetid' => $fleet->id])}}" class="btn btn-success">Join Fleet</a>
                     @break
