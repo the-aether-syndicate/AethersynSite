10e3fb703e27435bb9f09bde616bcd41e2ca2ce7
DAVIS <fdavis1@kleinisd.net>
2019-06-13 10:40:04 -0500

    add importys

--- database/migrations/2019_06_13_230239_create_page_table.php
+++ database/migrations/2019_06_13_230239_create_page_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreatePageTable extends Migration
 {
