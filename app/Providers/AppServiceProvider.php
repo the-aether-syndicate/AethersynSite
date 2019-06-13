9ad75430b112c1007e803fe1fe9b2eb2d5b59f1e
DAVIS <fdavis1@kleinisd.net>
2019-06-13 13:59:47 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -59,7 +59,7 @@ class AppServiceProvider extends ServiceProvider
                     'text' => $page['title'],
                     'url' => 'pages/' . $page['id']
                 ];
-            }).toArray();
+            })->toArray();
             $event->menu->add([
              'text' => 'Pages',
              'submenu'=> $pages
